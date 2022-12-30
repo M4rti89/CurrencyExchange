40357f8291f5e56ae19a2c34784cbd99601da808
Marcin Kubala <marcin.kubala.89@gmail.com>
2022-12-30 03:07:52 +0100

    renaming and code aesthetic improvements

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 {
     const init = () => {
         const formElement = document.querySelector(".js-form");
+
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
 
             const inputElement = document.querySelector(".js-input");
             const currencyElement = document.querySelector(".js-currency");
 
-            displayResult(calculate(currencyElement.value, inputElement.value), currencyElement.value);
+            updateResultText(calculateResult(currencyElement.value, inputElement.value), currencyElement.value);
         });
     }
 
-    const displayResult = (result, currency) => {
+    const updateResultText = (result, currency) => {
         const resultElement = document.querySelector(".js-result");
+
         resultElement.value = `${result.toFixed(2)} ${currency}`;
     }
 
-    const calculate = (currency, amount) => {
+    const calculateResult = (currency, amount) => {
 
         const USD = 4.46;
         const EUR = 4.68;
@@ -26,15 +28,12 @@
         switch (currency) {
             case "USD":
                 return amount / USD
-                break;
 
             case "EUR":
                 return amount / EUR
-                break;
 
             case "GBP":
                 return amount / GBP
-                break;
 
             case "CHF":
                 return amount / CHF
